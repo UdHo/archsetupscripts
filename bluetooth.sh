6650f9f2e0c368da57d79cea27578ec48cfe4194
Udo Hoffmann <hoffmann.odu@gmail.com>
2019-09-13 01:11:57 +0200

    Bluetooth

---
+++ bluetooth.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+systemctl start bluetooth.service
+systemctl enable bluetooth.service
+sudo sed -i "s/#\?AutoEnable=[a-z]*$/AutoEnable=true /g" /etc/bluetooth/main.conf
+
+echo "load-module module-bluetooth-policy\n load-module module-bluetooth-discover" | sudo tee "/etc/pulse/system.pa"
+
+echo 'SUBSYSTEM=="bluetooth", ACTION=="add", RUN+="/home/udo/.config/scripts/bt-auto-enable-a2dp.sh"' | sudo tee "/etc/udev/rules.d/20-bt-auto-enable-a2dp.rules" 
+
